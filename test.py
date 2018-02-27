d02e0767b28c27807e2283e1271091e2fcd10179
unknown <tio.qynxx@gmail.com>
2018-02-27 16:01:29 +0700

    random exercises

---
+++ test.py
@@ -0,0 +1,6 @@
+choice = input("Do you enjoy the trip ? (y/n)")
+
+while choice != 'n' and choice != 'y':
+    choice = input("Sorry I don't understand. Enter again: ")
+
+print ('Thank you for your feedback')
