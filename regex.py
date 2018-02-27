bf1b24f862246166a36e0318eb72633be332ad58
unknown <tio.qynxx@gmail.com>
2018-02-27 18:28:06 +0700

    learn regex more

--- regex.py
+++ regex.py
@@ -1,8 +1,17 @@
 import re
 
 def anti_vowel(text):
+  #replace every vowel with empty
   regex = r"([aiueoAIUEO]+)"
   text = re.sub(regex,'',text)
   return text
 
-print (anti_vowel("Hey What's up guys!"))
+print (anti_vowel("Hey What's up guyss!"))
+
+def censor(text, word):
+  #censor every 'hey word in the text/first argument'
+  regex = r"(" + word +")"
+  text = re.sub(regex, '*' * len(word), text)
+  return text
+
+print (censor("hey hey tey hey is hey hey", "hey"))
